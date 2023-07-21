db288e187467c9205d1e3a7d1884d59b39f53489
Samim Aktar <samimaktar0773@gmail.com>
2023-07-21 15:56:45 +0530

    complete donor form creatioon and validation

--- src/app/components/donor/registration/registration.component.ts
+++ src/app/components/donor/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,36 +7,51 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
+  submitted = false;
   donorData: any = FormGroup;
 
   ngOnInit() {
     this.donorData = new FormGroup({
-      name: new FormControl(null),
-      genter: new FormControl(),
-      mobile: new FormControl(null),
-      dob: new FormControl(null),
-      image: new FormControl(null),
-      bloodGroup: new FormControl(null),
-      accountStatus: new FormControl(null),
-      address: new FormControl(null),
-      city: new FormControl(null),
-      postalCode: new FormControl(null),
-      state: new FormControl(null),
-      conutry: new FormControl(null),
-      hosName: new FormControl(null),
-      hosAddress: new FormControl(null),
-      hosCity: new FormControl(null),
-      hosPostalCode: new FormControl(null),
-      hosState: new FormControl(null),
-      hosCountry: new FormControl(null),
-      password: new FormControl(null),
+      personalDetails: new FormGroup({ // formGrouping 
+        name: new FormControl(null, Validators.required),
+        mobile: new FormControl(null,Validators.required),
+        gender: new FormControl(null,Validators.required),
+        dob: new FormControl(null,Validators.required),
+        bloodGroup: new FormControl(null,Validators.required),
+      }),
+      residentialDetails: new FormGroup({
+        address: new FormControl(null,Validators.required),
+        city: new FormControl(null,Validators.required),
+        postalCode: new FormControl(null,Validators.required),
+        state: new FormControl(null,Validators.required),
+        conutry: new FormControl(null,Validators.required),
+      }),
+      hospitalDetails: new FormGroup({
+        hosName: new FormControl(null,Validators.required),
+        hosAddress: new FormControl(null,Validators.required),
+        hosCity: new FormControl(null,Validators.required),
+        hosPostalCode: new FormControl(null,Validators.required),
+        hosState: new FormControl(null,Validators.required),
+        hosCountry: new FormControl(null,Validators.required),
+      }),
+      accountDetails: new FormGroup({
+        accountStatus: new FormControl(null,Validators.required),
+        password: new FormControl(null,Validators.required),
+      }),
     });
 
   }
 
-  onSubmit(){
+
+
+  onSubmit() {
+    this.submitted =true;
+    if(this.donorData.invalid){
+      return
+    }
+    alert("scces")
     console.log(this.donorData);
+    console.log(this.donorData.value);
   }
 
 }
